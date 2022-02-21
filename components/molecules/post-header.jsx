e5509eb8eade98b3b084b406bcc8f0c453aaaa73
wilson <wildiscjockey@gmail.com>
2022-02-21 18:53:53 -0400

    Edit: CSS

--- components/molecules/post-header.jsx
+++ components/molecules/post-header.jsx
@@ -8,9 +8,9 @@ export default function PostHeader({ title, date, author, excerpt }) {
     <>
       <div className="dark:bg-background-dark py-8 lg:pb-10">
         <PostTitle>{title}</PostTitle>
-        <h2 className="text-lg font-sans px-8 md:px-32 lg:px-56 xl:px-80 pb-4 dark:text-secondary-dark">{excerpt}</h2>
+        <h2 className="text-lg font-sans px-8 md:px-32 lg:px-56 xl:px-80 pb-4 dark:text-secondary-dark text-primary-text">{excerpt}</h2>
         <div className="mx-8 md:mx-32 lg:mx-56 xl:mx-80">
-          <div className="flex place-items-center justify-between text-md text-secondary-text dark:text-tertiary-dark">
+          <div className="flex place-items-center justify-between text-md text-tertiary-text dark:text-tertiary-dark">
             <div className="flex items-center">
               <Avatar name={author.name} picture={author.picture} />
               <span className="ml-3 lg:ml-4 mr-1">Escrito por:

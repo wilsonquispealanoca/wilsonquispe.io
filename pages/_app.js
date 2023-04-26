6d40398fc1d66362cf4f41a4be0a4d156c667eac
wilsonquispealanoca <wildiscjockey@gmail.com>
2023-04-26 11:00:18 +0000

    ADD: microsoft clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
-import { ThemeProvider } from 'next-themes'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "gu3w4acz8x");
+          `,
+        }}
+      />
       <Component {...pageProps} />
-    </ThemeProvider>
+    </> 
   )
 }
 
--- pages/historias.jsx
+++ pages/historias.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Activelink from "../components/atoms/activeLink";
 import Container from "../components/templates/container";
 import Layout from "../components/templates/layout";
@@ -10,15 +11,15 @@ export default function Historias() {
         <>
             <div className="flex items-center justify-center">
                 <div className="max-w-2xl w-full px-4">
-                    <h1 className="text-4xl font-bold text-center mb-8">¡Muy pronto estará listo!</h1>
+                    <h1 className="text-4xl font-bold text-center mb-8 text-white">¡Muy pronto estará listo!</h1>
                     <p className="text-lg text-gray-600 text-center mb-12">Está sección aún la voy construyendo. ¡Volveré pronto!
                     </p>
                     {/* <form className="flex flex-col md:flex-row justify-center items-center gap-4">
                         <input className="w-full md:w-80  py-2 px-4 border text-gray-800 border-gray-200 bg-white" type="email" placeholder="Ingresa tu correo electrónico"/>
                         <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 border">Notifícame cuando este listo</button>
                     </form> */}
-                    <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 border">
-                        <Activelink href="/">Volver al Inicio</Activelink>
+                    <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 border w-full">
+                        <Link href="/">Volver al inicio</Link>
                     </button>
                 </div>
             </div>

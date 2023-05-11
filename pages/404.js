70a67e6ba86a21125f3ed21d0711fe8ca0481dc1
wilsonquispealanoca <wildiscjockey@gmail.com>
2023-05-10 20:26:59 -0400

    UPDATE: cta pages

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import cat404 from "../public/catronaut.svg";
-
 export default function Custom404() {
   return (
     <div className="h-screen w-screen bg-[#14102c] flex items-center">
       <div className="flex flex-col md:flex-row items-center justify-center mx-auto">
         <div className="max-w-lg px-6">
-          <div className="text-3xl lg:text-5xl text-brand-beige font-averia font-bold my-10 lg:mb-16">
+          <div className="text-4xl lg:text-5xl text-brand-beige font-averia font-bold my-10 lg:mb-16">
             Parece que estás perdido
           </div>
-          <p className="text-lg lg:text-2xl md:text-3xl font-light text-brand-beige font-outfit mb-3 lg:mb-6">
+          <p className="text-2xl md:text-3xl font-light text-brand-beige font-outfit mb-8 lg:mb-6">
             Quédate donde estas, enviaremos a alguien a buscarte
           </p>
           <Link href="/">
-            <button className="px-4 inline py-2 text-sm lg:text-lg text-black font-outfit rounded-md w-full lg:w-64 focus:outline-none focus:shadow-outline-blue bg-brand-yellow">
+            <button
+              className="px-6 py-4 bg-brand-yellow rounded-full uppercase font-black
+             border border-black cursor-pointer text-brand-charcoal font-outfit"
+            >
               Regresar al inicio
             </button>
           </Link>
--- pages/wp-admin.js
+++ pages/wp-admin.js
@@ -2,25 +2,28 @@ import Link from "next/link";
 
 export default function Wpadmin() {
   return (
-    <div class="bg-indigo-900 relative overflow-hidden h-screen">
+    <div className="bg-indigo-900 relative overflow-hidden h-screen">
       <img
         src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
-        class="absolute h-full w-full object-cover"
+        className="absolute h-full w-full object-cover"
         alt="background wp-admin"
       />
-      <div class="inset-0 bg-black opacity-25 absolute"></div>
-      <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-6 xl:py-32">
-        <div class="w-full font-mono flex flex-col items-center relative z-10">
-          <h1 class="font-averia text-3xl lg:text-6xl text-center text-brand-beige lg:leading-tight">
+      <div className="inset-0 bg-black opacity-25 absolute"></div>
+      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-6 xl:py-32">
+        <div className="w-full font-mono flex flex-col items-center relative z-10">
+          <h1 className="font-averia text-5xl text-center text-brand-beige lg:leading-tight">
             ¿Parece que andabas buscando algo aquí?
           </h1>
           <div className="bg-black/70 mt-24 lg:mt-32 p-4 lg:p-20 rounded-md">
-            <p class="font-outfit text-xl text-brand-beige text-center mb-4 lg:mb-8">
+            <p className="font-outfit text-xl text-brand-beige text-center mb-4 lg:mb-8">
               Te invito a que leas mi publicación donde menciono que tecnologias
               usé en mi blog.
             </p>
             <Link href="https://wilsonquispe.vercel.app/posts/rebuilding-blog">
-              <button className="px-4 inline py-2 text-sm lg:text-lg text-black font-outfit rounded-md w-full focus:outline-none focus:shadow-outline-blue bg-brand-yellow">
+              <button
+                className="px-6 py-4 bg-brand-yellow rounded-full uppercase font-black
+             border border-black cursor-pointer text-brand-charcoal font-outfit"
+              >
                 Quiero leer la publicación
               </button>
             </Link>

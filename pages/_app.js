fbba43147018f64ec5643522acb6042abf174d14
wilsonquispealanoca <wildiscjockey@gmail.com>
2023-06-13 17:40:09 -0400

    UPDATE: collect api

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* Microsoft Clarity */}
-      <Script id="clarity" strategy="lazyOnload">
-        {`
-                    (function(c,l,a,r,i,t,y){
-                      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                  })(window, document, "clarity", "script", 'gu3w4acz8x');
-                `}
+      <Script id="microsoftofclarity" strategy="lazyOnload">
+        {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", 'gu3w4acz8x');
+        `}
       </Script>
 
       {/* Google analitycs */}

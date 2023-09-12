05376dc655c40e5bca7799f9999a1415633225f8
wilsonquispealanoca <wildiscjockey@gmail.com>
2023-09-12 17:17:50 -0400

    ADD: second story

--- components/molecules/congratulationsStory.jsx
+++ components/molecules/congratulationsStory.jsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
+//javascript audio library
+import { Howl } from "howler";
 
 export const CongratulationsStory = ({ isCorrect }) => {
+  const soundTriumphant = new Howl({
+    src: ["/sounds/triumphant.mp3"], // Ruta de tu archivo de audio
+  });
+  soundTriumphant.play();
   return (
     <div className="bg__lines-diagonals relative z-10 flex items-center justify-center h-screen w-full">
       <div className="bg__congratulation-degrade z-20 flex items-center justify-center flex-col px-4 h-screen">
         <div className="w-full h-48 lg:h-64 relative">
           {isCorrect && (
             <Image
-              src="/assets/illustrations/achaku1.svg"
+              src="/assets/illustrations/warmi.svg"
               alt="iconWin"
               fill
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
@@ -17,7 +23,7 @@ export const CongratulationsStory = ({ isCorrect }) => {
           )}
           {isCorrect === false && (
             <Image
-              src="/assets/illustrations/achaku2.svg"
+              src="/assets/illustrations/faceKid.svg"
               alt="iconLose"
               fill
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
@@ -29,10 +35,10 @@ export const CongratulationsStory = ({ isCorrect }) => {
           ¡Si que estás dominando tus lecciones de aymara hoy!
         </h1>
         <h2 className="font-outfitsemibold text-red-900 my-6 text-center  text-2xl md:text-2xl">{`${
-          isCorrect ? "Y sí, él es Remi" : "Y no, él no es Remi "
+          isCorrect ? "Y sí, ella es Justina" : "Y no, él es mario "
         }`}</h2>
         <p className="text-red-900 font-outfit text-center text-lg lg:text-xl mb-4">
-          Hoy hiciste nuevos amigos, ya conoces a Remi y Emil
+          Hoy hiciste nuevos amigos, ya conoces a Justina y Mario
         </p>
         <div className="flex flex-col gap-2 w-full">
           <Link href="/blog/" className="w-full flex justify-center">

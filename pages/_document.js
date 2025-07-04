fb44fa183ca381d5fb7ba02884efb11d32c8de84
wilsonquispealanoca <wildiscjockey@gmail.com>
2025-07-03 22:38:40 -0400

    UPDATE: clarity

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "gu3w4acz8x");`
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

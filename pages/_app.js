d5763149a31f4622bc0bbf54ace130cd23cf776f
wilsonquispealanoca <wildiscjockey@gmail.com>
2025-07-03 20:51:45 -0400

    UPDATE: microsoft clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Script from "next/script";
+import Script from 'next/script'
 import "../styles/globals.css";
 
 import i18n from "i18next";
@@ -33,12 +33,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* Microsoft Clarity */}
-      <Script id="microsoftofclarity" strategy="lazyOnload">
+      <Script id="microsoftofclarity" strategy="afterInteractive">
         {`(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", 'gu3w4acz8x');
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gu3w4acz8x");
         `}
       </Script>
 

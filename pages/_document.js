fd356b0bca2eb466fa913947ca351ff3965cb03c
wilsonquispealanoca <wildiscjockey@gmail.com>
2025-11-16 17:09:55 -0400

    ADD: Helen's history

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head />
-      <body>
+      <body className="overflow-x-hidden">
         <Main />
         <NextScript />
       </body>

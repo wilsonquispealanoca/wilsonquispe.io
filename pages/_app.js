1d95c66dda3fe0f29451db97ae824573e136dd76
wilsonquispealanoca <wildiscjockey@gmail.com>
2026-02-21 17:27:22 -0400

    UPDATE: histories

--- pages/_app.js
+++ pages/_app.js
@@ -35,14 +35,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* Microsoft Clarity */}
-      <Script id="microsoftofclarity" strategy="afterInteractive">
-        {`(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "gu3w4acz8x");
-        `}
-      </Script>
+      <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "gu3w4acz8x");
+      </script>
 
       {/* Google analitycs */}
       <Script
